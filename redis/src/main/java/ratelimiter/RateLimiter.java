e99bc9c29dd2b0003512dfc3cfb8c9f5e55f29c5
Garmash Maxim <garmash.maxim2020@gmail.com>
2026-03-19 10:55:40 +0300

    Implement pass() method using sliding window algorithm

--- redis/src/main/java/ratelimiter/RateLimiter.java
+++ redis/src/main/java/ratelimiter/RateLimiter.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.time.Instant;
+
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
@@ -22,8 +23,20 @@ public class RateLimiter {
   }
 
   public boolean pass() {
-    // TODO: Implementation
-    return false;
+    long timeNow = Instant.now().toEpochMilli();
+    long windowStart = timeNow - timeWindowSeconds * 1000;
+
+    redis.zremrangeByScore(label, 0, windowStart);
+    long requestCount = redis.zcount(label, windowStart, timeNow);
+
+    if (requestCount >= maxRequestCount) {
+      return false;
+    }
+
+    redis.zadd(label, timeNow, String.valueOf(timeNow));
+    redis.expire(label, (int) timeWindowSeconds);
+
+    return true;
   }
 
   public static void main(String[] args) {
